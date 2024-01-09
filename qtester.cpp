817f4cba456f735ca5b155cf4da5dcfdb8a4fac8
vinicius <andrade.vinicius934@gmail.com>
2024-01-09 15:07:35 -0300

    format, ds_store, etc

--- qtester.cpp
+++ qtester.cpp
@@ -1,15 +1,15 @@
 #include "qtester.h"
-#include "ui_qtester.h"
-#include <QFile>
-#include <QDataStream>
-#include <QLatin1StringView>
-#include <QScrollBar>
-#include <QKeyEvent>
-#include <algorithm>
-#include <QBluetoothSocket>
 #include <QBluetoothDeviceDiscoveryAgent>
 #include <QBluetoothDeviceInfo>
 #include <QBluetoothLocalDevice>
+#include <QBluetoothSocket>
+#include <QDataStream>
+#include <QFile>
+#include <QKeyEvent>
+#include <QLatin1StringView>
+#include <QScrollBar>
+#include "ui_qtester.h"
+#include <algorithm>
 
 constexpr auto MAX_BUFFER_SIZE = qsizetype(256);
 
@@ -39,8 +39,10 @@ QTester::QTester(QWidget* parent)
         }
     });
 
-    connect(mDiscoveryAgent, &QBluetoothDeviceDiscoveryAgent::errorOccurred, this, [&] (QBluetoothDeviceDiscoveryAgent::Error error) {
-        mUI->statusBar->showMessage(QString("Erro na descoberta - %0 [%1]").arg(mDiscoveryAgent->errorString()).arg((int)mDiscoveryAgent->error()));
+    connect(mDiscoveryAgent, &QBluetoothDeviceDiscoveryAgent::errorOccurred, this, [&](QBluetoothDeviceDiscoveryAgent::Error error) {
+        mUI->statusBar->showMessage(QString("Erro na descoberta - %0 [%1]")
+                                        .arg(mDiscoveryAgent->errorString())
+                                        .arg(static_cast<int>(mDiscoveryAgent->error())));
         // error when discovering devices, could be because of a sudden disconnection
         // for now just ignore it and search again
         // FIXME: handle the error?
@@ -52,12 +54,12 @@ QTester::QTester(QWidget* parent)
         // if we are connected or in the process of connecting don't do anything
         // otherwise search again
         switch (mSocket->state()) {
-            case QBluetoothSocket::SocketState::ConnectedState:
-            case QBluetoothSocket::SocketState::ConnectingState:
-                qDebug() << "discovery finshed";
-                break;
-            default:
-                searchForDevice();
+        case QBluetoothSocket::SocketState::ConnectedState:
+        case QBluetoothSocket::SocketState::ConnectingState:
+            qDebug() << "discovery finshed";
+            break;
+        default:
+            searchForDevice();
         }
     });
 
@@ -70,7 +72,7 @@ QTester::QTester(QWidget* parent)
         searchForDevice();
     });
 
-    connect(mSocket, &QBluetoothSocket::errorOccurred, this, [&] (QBluetoothSocket::SocketError error) {
+    connect(mSocket, &QBluetoothSocket::errorOccurred, this, [&](QBluetoothSocket::SocketError error) {
         mUI->statusBar->showMessage(QString("Erro na socket - %0 [%1]").arg(mSocket->errorString()).arg((int)mSocket->error()));
         searchForDevice();
     });
@@ -79,123 +81,70 @@ QTester::QTester(QWidget* parent)
 
     connect(mUI->clearLogSerial, &QPushButton::clicked, mUI->logSerial, &QTextEdit::clear);
 
-    const auto updatePumpText = [this] (bool state) {
+    const auto updatePumpText = [this](bool state) {
         mUI->testPump->setText((state ? "Desligar" : "Ligar") + QString(" Bomba"));
     };
 
     updatePumpText(false);
-    connect(
-        mUI->testPump,
-        &QPushButton::clicked,
-        this,
-        [this, updatePumpText] {
-            static bool s_state = true;
+    connect(mUI->testPump, &QPushButton::clicked, this, [this, updatePumpText] {
+        static bool s_state = true;
 
-            auto gcode = QString("$L5 T0 S%1 V%2$").arg(s_state).arg(mUI->pumpDigitalValue->value());
-            qDebug() << gcode;
-            sendRawBufferToMachine(gcode.toLatin1());
-            updatePumpText(s_state);
+        auto gcode = QString("$L5 T0 S%1 V%2$").arg(s_state).arg(mUI->pumpDigitalValue->value());
+        qDebug() << gcode;
+        sendRawBufferToMachine(gcode.toLatin1());
+        updatePumpText(s_state);
 
-            s_state = not s_state;
-        });
+        s_state = not s_state;
+    });
 
     static auto resistances = std::array{ mUI->testResistance1, mUI->testResistance2, mUI->testResistance3 };
     static auto resistancesPins = std::array{ 69, 16, 0 };
     static auto resistancesPwm = std::array{ mUI->testResistancePwm1, mUI->testResistancePwm2, mUI->testResistancePwm3 };
     static auto resistancesNames = std::array{ QString("TH0"), QString("TH1"), QString("Bed") };
 
-    const auto updateResistanceText = [this] (QPushButton* resistance, QString name, bool state) {
+    const auto updateResistanceText = [this](QPushButton* resistance, QString name, bool state) {
         resistance->setText((state ? "Desligar " : "Ligar ") + name);
     };
 
     for (int i = 0; i < 3; ++i) {
         updateResistanceText(resistances[i], resistancesNames[i], false);
-        connect(
-            resistances[i],
-            &QPushButton::clicked,
-            this,
-            [&, index = i, state = true] () mutable {
+        connect(resistances[i], &QPushButton::clicked, this, [&, index = i, state = true]() mutable {
+            auto gcode = QString("$L6 P%1 M1 V%2 W$").arg(resistancesPins[index]).arg(state ? resistancesPwm[index]->value() : 0);
+            sendRawBufferToMachine(gcode.toLatin1());
+            updateResistanceText(resistances[index], resistancesNames[index], state);
 
-                auto gcode = QString("$L6 P%1 M1 V%2 W$").arg(resistancesPins[index]).arg(state ? resistancesPwm[index]->value() : 0);
-                qDebug() << gcode;
-                sendRawBufferToMachine(gcode.toLatin1());
-                updateResistanceText(resistances[index], resistancesNames[index], state);
-
-                state = not state;
-            });
+            state = not state;
+        });
     }
 
-    connect(
-        mUI->testBeeper,
-        &QPushButton::pressed,
-        this,
-        [this] { sendRawBufferToMachine(QString("$L5 T6 S1 V%1$").arg(mUI->beeperFrequency->value()).toLatin1()); });
+    connect(mUI->testBeeper, &QPushButton::pressed, this, [this] { sendRawBufferToMachine(QString("$L5 T6 S1 V%1$").arg(mUI->beeperFrequency->value()).toLatin1()); });
 
-    connect(
-        mUI->testBeeper,
-        &QPushButton::released,
-        this,
-        [this] { sendRawBufferToMachine("$L5 T6 S0$"); });
+    connect(mUI->testBeeper, &QPushButton::released, this, [this] { sendRawBufferToMachine("$L5 T6 S0$"); });
 
-    connect(
-        mUI->logFlowSensor,
-        &QCheckBox::clicked,
-        this,
-        [this] {
-            auto gcode = QString("$L5 T4 S%1$").arg(int(mUI->logFlowSensor->isChecked()));
-            sendRawBufferToMachine(gcode.toLatin1());
-        });
+    connect(mUI->logFlowSensor, &QCheckBox::clicked, this, [this] {
+        auto gcode = QString("$L5 T4 S%1$").arg(int(mUI->logFlowSensor->isChecked()));
+        sendRawBufferToMachine(gcode.toLatin1());
+    });
 
-    connect(
-        mUI->logTemperature,
-        &QCheckBox::clicked,
-        this,
-        [this] {
-            auto gcode = QString("$L5 T5 S%1$").arg(int(mUI->logTemperature->isChecked()));
-            sendRawBufferToMachine(gcode.toLatin1());
-        });
+    connect(mUI->logTemperature, &QCheckBox::clicked, this, [this] {
+        auto gcode = QString("$L5 T5 S%1$").arg(int(mUI->logTemperature->isChecked()));
+        sendRawBufferToMachine(gcode.toLatin1());
+    });
 
-    connect(
-        mUI->heatUpWater,
-        &QCheckBox::clicked,
-        this,
-        [this] {
-            auto gcode = QString("$L5 T7 S%1$").arg(int(mUI->heatUpWater->isChecked()));
-            sendRawBufferToMachine(gcode.toLatin1());
-        });
+    connect(mUI->heatUpWater, &QCheckBox::clicked, this, [this] {
+        auto gcode = QString("$L5 T7 S%1$").arg(int(mUI->heatUpWater->isChecked()));
+        sendRawBufferToMachine(gcode.toLatin1());
+    });
 
-    connect(
-        mUI->xAxisLeft,
-        &QToolButton::clicked,
-        this,
-        [this] { sendRawBufferToMachine("$G0 X-1$"); });
+    connect(mUI->xAxisLeft, &QToolButton::clicked, this, [this] { sendRawBufferToMachine("$G0 X-1$"); });
 
-    connect(
-        mUI->xAxisRight,
-        &QToolButton::clicked,
-        this,
-        [this] { sendRawBufferToMachine("$G0 X1$"); });
+    connect(mUI->xAxisRight, &QToolButton::clicked, this, [this] { sendRawBufferToMachine("$G0 X1$"); });
 
-    connect(
-        mUI->yAxisDown,
-        &QToolButton::clicked,
-        this,
-        [this] { sendRawBufferToMachine("$G0 Y-1$"); });
+    connect(mUI->yAxisDown, &QToolButton::clicked, this, [this] { sendRawBufferToMachine("$G0 Y-1$"); });
 
-    connect(
-        mUI->yAxisUp,
-        &QToolButton::clicked,
-        this,
-        [this] { sendRawBufferToMachine("$G0 Y1$"); });
+    connect(mUI->yAxisUp, &QToolButton::clicked, this, [this] { sendRawBufferToMachine("$G0 Y1$"); });
 
-    connect(
-        mUI->home,
-        &QPushButton::clicked,
-        this,
-        [this] {
-            auto gcode = QString("$G28 XY$");
-            sendRawBufferToMachine(gcode.toLatin1());
-        });
+    connect(mUI->home, &QPushButton::clicked, this, [this] { sendRawBufferToMachine("$G28 XY$"); });
 
     const auto changeLedText = [this](int index) {
         auto str1 = QString("Testar LED %1").arg(index + 1);
@@ -208,38 +157,22 @@ QTester::QTester(QWidget* parent)
 
     changeLedText(mUI->selectedStation->currentIndex());
 
-    connect(
-        mUI->selectedStation,
-        &QComboBox::currentIndexChanged,
-        this,
-        changeLedText);
+    connect(mUI->selectedStation, &QComboBox::currentIndexChanged, this, changeLedText);
 
-    connect(
-        mUI->testButtonLed,
-        &QPushButton::clicked,
-        this,
-        [this] {
-            auto gcode = QString("$L5 T2 V%1$").arg(mUI->selectedStation->currentIndex());
-            sendRawBufferToMachine(gcode.toLatin1());
-        });
+    connect(mUI->testButtonLed, &QPushButton::clicked, this, [this] {
+        auto gcode = QString("$L5 T2 V%1$").arg(mUI->selectedStation->currentIndex());
+        sendRawBufferToMachine(gcode.toLatin1());
+    });
 
-    connect(
-        mUI->testPowerLed,
-        &QPushButton::clicked,
-        this,
-        [this] {
-            auto gcode = QString("$L5 T3 V%1$").arg(mUI->selectedStation->currentIndex());
-            sendRawBufferToMachine(gcode.toLatin1());
-        });
+    connect(mUI->testPowerLed, &QPushButton::clicked, this, [this] {
+        auto gcode = QString("$L5 T3 V%1$").arg(mUI->selectedStation->currentIndex());
+        sendRawBufferToMachine(gcode.toLatin1());
+    });
 
-    connect(
-        mUI->travelToStation,
-        &QPushButton::clicked,
-        this,
-        [this] {
-            auto gcode = QString("$L3 N%1$").arg(mUI->selectedStation->currentIndex());
-            sendRawBufferToMachine(gcode.toLatin1());
-        });
+    connect(mUI->travelToStation, &QPushButton::clicked, this, [this] {
+        auto gcode = QString("$L3 N%1$").arg(mUI->selectedStation->currentIndex());
+        sendRawBufferToMachine(gcode.toLatin1());
+    });
 
     const auto changePinCommandText = [this](int index) {
         bool is_output = index == 0x1;
@@ -249,74 +182,52 @@ QTester::QTester(QWidget* parent)
     };
 
     changePinCommandText(mUI->pinCfg->currentIndex());
-    connect(
-        mUI->pinCfg,
-        &QComboBox::currentIndexChanged,
-        this,
-        changePinCommandText);
+    connect(mUI->pinCfg, &QComboBox::currentIndexChanged, this, changePinCommandText);
 
-    connect(
-        mUI->sendPinCommand,
-        &QPushButton::clicked,
-        this,
-        [this] {
-            const bool is_output = mUI->pinCfg->currentIndex() == 0x1;
-            auto text = mUI->pinName->text();
-            if (text.isEmpty() || text.size() < 3 || text.front() != 'P' || (text[1] < 'A' || text[1] > 'E'))
-                return;
+    connect(mUI->sendPinCommand, &QPushButton::clicked, this, [this] {
+        const bool is_output = mUI->pinCfg->currentIndex() == 0x1;
+        auto text = mUI->pinName->text();
+        if (text.isEmpty() || text.size() < 3 || text.front() != 'P' || (text[1] < 'A' || text[1] > 'E'))
+            return;
 
-            auto pin_number = [this] {
-                const auto latin1 = mUI->pinName->text().toLatin1();
+        auto pin_number = [this] {
+            const auto latin1 = mUI->pinName->text().toLatin1();
 
-                bool ok;
-                const auto pin_id = latin1.sliced(2).toInt(&ok);
-                if (not ok || pin_id > 15)
-                    return -1;
+            bool ok;
+            const auto pin_id = latin1.sliced(2).toInt(&ok);
+            if (not ok || pin_id > 15)
+                return -1;
 
-                const auto letter_number = latin1[1] - 'A';
-                return letter_number * 15 + letter_number + pin_id;
-            }();
+            const auto letter_number = latin1[1] - 'A';
+            return letter_number * 15 + letter_number + pin_id;
+        }();
 
-            if (pin_number == -1)
-                return;
+        if (pin_number == -1)
+            return;
 
-            if (is_output) {
-                auto gcode = QString("$L6 P%1 M%2 V%3 W$")
-                                 .arg(pin_number)
-                                 .arg(mUI->pinCfg->currentIndex())
-                                 .arg(mUI->pinOutputValue->value());
-                sendRawBufferToMachine(gcode.toLatin1());
-            } else {
-                auto gcode = QString("$L6 P%1 M%2 R$")
-                                 .arg(pin_number)
-                                 .arg(mUI->pinCfg->currentIndex());
-                sendRawBufferToMachine(gcode.toLatin1());
-            }
-        });
+        if (is_output) {
+            auto gcode = QString("$L6 P%1 M%2 V%3 W$")
+                             .arg(pin_number)
+                             .arg(mUI->pinCfg->currentIndex())
+                             .arg(mUI->pinOutputValue->value());
+            sendRawBufferToMachine(gcode.toLatin1());
+        } else {
+            auto gcode = QString("$L6 P%1 M%2 R$")
+                             .arg(pin_number)
+                             .arg(mUI->pinCfg->currentIndex());
+            sendRawBufferToMachine(gcode.toLatin1());
+        }
+    });
 
 #ifndef ANDROID
     mUI->flexButton->setText("Atualizar Firmware");
-    connect(
-        mUI->flexButton,
-        &QPushButton::clicked,
-        this,
-        &QTester::startFirmwareUpdate);
+    connect(mUI->flexButton, &QPushButton::clicked, this, &QTester::startFirmwareUpdate);
 #else
     mUI->flexButton->setText("Enviar Comando");
-    connect(
-        mUI->flexButton,
-        &QPushButton::clicked,
-        this,
-        &QTester::sendUserInput);
+    connect(mUI->flexButton, &QPushButton::clicked, this, &QTester::sendUserInput);
 #endif
 
-    connect(
-        mUI->leaveTesterMode,
-        &QPushButton::clicked,
-        this,
-        [this] {
-            sendRawBufferToMachine("$L4 K0$");
-        });
+    connect(mUI->leaveTesterMode, &QPushButton::clicked, this, [this] { sendRawBufferToMachine("$L4 K0$"); });
 
     mUI->statusBar->showMessage("Procurando...");
     searchForDevice();
@@ -373,7 +284,7 @@ void QTester::sendUserInput() {
         qDebug() << "cade o delimitador";
         mUI->userInput->clear();
         return;
-    }    
+    }
 
     if (input.back() != input.front())
         input.push_back(input.front());
